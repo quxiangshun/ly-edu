d4c834dca3d11dd186407ab6ec78c663b1473e94
quxiangshun <quxiangshun@gmail.com>
2026-02-08 09:12:37 +0800

    fix: MySQL添加初始化，可以远程登录

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,4 +1,9 @@
 -- LyEdu 数据库初始化脚本
+-- 允许从本机客户端（经 Docker 端口转发）连接：root@'%' + 传统认证，便于 Navicat/DBeaver 等连接
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'lyedu123456';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'lyedu123456';
+FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS `lyedu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
